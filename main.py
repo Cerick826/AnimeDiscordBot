be32b97b4f4e37c7f50c171c972e9185cb3c7879
williamye51 <31286548+williamye51@users.noreply.github.com>
2022-02-22 12:53:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ async def on_message(message):
     await bot.process_commands(message)
     
 # COMMANDS
+
 @bot.command(name="saveList", aliases=["savelist", "Savelist", "SaveList"], pass_context=True)
 async def saveList(ctx, *, arg = None):
     if(arg == None):
@@ -80,26 +81,7 @@ async def delAnime(ctx, *, arg = None):
     if(arg == None):
         await ctx.send("Incorrect Usage: !delAnime <anime name>")
         return
-      
-@bot.command(name="saveList", aliases=["savelist", "Savelist", "SaveList"], pass_context=True)
-async def saveList(ctx, *, arg):
-    my_id = str(ctx.message.author.id)
-    cur.execute(f"SELECT animelist FROM watchlist WHERE user_id = {my_id}")
-    result = cur.fetchall()
-    mylist = " ".join(map(str, result))
-    print(mylist)
-    if len(mylist) == 5:
-        mylist = str(arg)
-    else:
-        mylist = mylist[2:-3]
-        mylist += ", " + str(arg)
-    print(mylist)
-    cur.execute("""UPDATE watchlist SET animelist= %s WHERE user_id = %s""", (mylist, my_id))
-    conn.commit()
-    await ctx.send(arg + " saved to list")
 
-@bot.command(name="showList", aliases=["showlist", "ShowList", "Showlist"], pass_context=True)
-async def showList(ctx):
     my_id = str(ctx.message.author.id)
     cur.execute(f"SELECT animelist FROM watchlist WHERE user_id = {my_id}")
     result = cur.fetchall()
@@ -155,6 +137,7 @@ async def deleteList(ctx):
             await ctx.send("List deleted!")
     else:
         await ctx.send("You don't have any list saved!")
+
 
 @bot.command(name="help", aliases=["Help"], pass_context=True)
 async def help(ctx, *, arg = None):
@@ -213,7 +196,7 @@ async def help(ctx, *, arg = None):
         embeddelAnime.add_field(name='Usage:',value='`!delAnime <anime name>`', inline = False)
         await ctx.send(embed=embeddelAnime)
         return
-    
+
     elif(arg == "deleteList" or arg == "deletelist" or arg == "Deletelist" or arg == "DeleteList"):
         # await ctx.send("deleteList Usage: !deleteList")
         embeddeleteList = discord.Embed(
@@ -236,79 +219,7 @@ async def help(ctx, *, arg = None):
     embed.set_footer(text=f'Requested by - {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name='Commands:',value=' `createList`, `saveList`, `showList`, `delAnime`, `deleteList`, `recommend`, `.......`')
     embed.add_field(name='Details:',value='`commandDetails`', inline = False)
-    embed.add_field(name='Command Usage Information',value='`Usage: !help <command>`', inline = False)
     await ctx.send(embed=embed)
     
-        await ctx.send(mylist)
-
-@bot.command(name="delAnime", aliases=["Delanime", "DelAnime", "delanime"], pass_context=True)
-async def delAnime(ctx, *, arg):
-    my_id = str(ctx.message.author.id)
-    cur.execute(f"SELECT animelist FROM watchlist WHERE user_id = {my_id}")
-    result = cur.fetchall()
-    mylist = " ".join(map(str, result))
-    if len(mylist) == 5:
-        print(mylist)
-        await ctx.send("The list is empty!")
-    else:
-        mylist = mylist[2:-3]
-        print(mylist)
-        mylist = mylist.replace(arg, "")
-        print(mylist[-2:-2])
-        if mylist[-2:-1] == ",":
-            mylist = mylist[:-2]
-        if mylist[0:1] == ",":
-            mylist = mylist[2:]
-        mylist = mylist.replace(", ,", ",")
-        cur.execute("""UPDATE watchlist SET animelist= %s WHERE user_id = %s""", (mylist, my_id))
-        conn.commit()
-        await ctx.send(arg + " deleted from anime list!")
-        
-@bot.command(name="createList", aliases=["Createlist", "CreateList", "createlist"], pass_context=True)
-async def createList(ctx):
-    author_id = str(ctx.message.author.id)
-    cur.execute(f"select user_id from watchlist where user_id = {author_id}")
-    find_id = cur.fetchall()
-
-    if len(find_id) != 0:
-        await ctx.send("You already have a list saved!")
-        pass
-    else:
-        sqladd = "INSERT INTO watchlist (user_id, animelist) VALUES (%s, %s)"
-        valadd = (author_id, " ")
-        cur.execute(sqladd, valadd)
-        await ctx.send("New watch list created!")
-        conn.commit()
-
-@bot.command(name="deleteList", aliases=["Deletelist", "DeleteList", "deletelist"], pass_context=True)
-async def deleteList(ctx):
-    author_id = str(ctx.message.author.id)
-    cur.execute(f"select user_id from watchlist where user_id = {author_id}")
-    find_id = cur.fetchall()
-    if len(find_id) != 0:
-        cur.execute(f"SELECT animelist FROM watchlist WHERE user_id = {author_id}")
-        result = cur.fetchall()
-        mylist = " ".join(map(str, result))
-        if len(mylist) == 5:
-            await ctx.send("List is empty")
-            pass
-        else:
-            cur.execute("""UPDATE watchlist SET animelist= %s WHERE user_id = %s""", ("", author_id))
-            conn.commit()
-            await ctx.send("List deleted!")
-    else:
-        await ctx.send("You don't have any list saved!")
-
-@bot.command(name="help", aliases=["Help"], pass_context=True)
-async def help(ctx):
-    embed = discord.Embed(
-        title = '**Help Menu**',
-        description = 'Use any of the following commands:',
-        color = discord.Color.blue()
-    )
-    embed.set_footer(text=f'Requested by - {ctx.author}', icon_url = ctx.author.avatar_url)
-    embed.add_field(name='Commands:',value=' `createList`, `saveList`, `showList`, `delAnime`, `deleteList`, `recommend`, `.......`')
-    embed.add_field(name='Details:',value='`commandDetails`', inline = False)
-    await ctx.send(embed=embed)
     
 bot.run('OTQyMjgwNzE5NjU1Mzk1MzY5.YgiNTg.e1knou32SWUBoL7iY4p6PcKHETQ')
