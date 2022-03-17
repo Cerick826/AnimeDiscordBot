1fb80f75be1aaf38e7f62ee3e71e7da319c68361
Vong Chen <43527143+kearkyle@users.noreply.github.com>
2022-03-17 14:36:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import discord
+import asyncio
 from discord import Embed
 from discord.ext import commands
 from discord_components import *
 from utils import sortWatchList
 from operator import truediv
-import datetime
-import asyncio
 import mysql.connector
+import datetime
+
 
 bot = commands.Bot(command_prefix='!', help_command=None)
 bot.remove_command('help')
@@ -20,7 +21,6 @@ async def on_ready():
     print('Bot is ready and Online')
     global botON
     botON = True
-
 
 @bot.event
 async def on_message(message):
@@ -61,9 +61,9 @@ async def menu(ctx):
     interaction = await bot.wait_for("button_click", check=lambda i: i.custom_id == "button0")
     await interaction.send(content="Button clicked!", ephemeral=False)
 
+
 @bot.command(name="saveList", aliases=["savelist", "Savelist", "SaveList"], pass_context=True)
 async def saveList(ctx, *, arg):
-    
     my_id = str(ctx.message.author.id)
     cur.execute(f"SELECT animelist FROM watchlist WHERE user_id = {my_id}")
     result = cur.fetchall()
@@ -82,11 +82,6 @@ async def saveList(ctx, *, arg):
     embed.add_field(name="Success!", value=f"{arg} -- saved to list", inline=False)
     await ctx.send(embed=embed)
 
-
-async def sortWatchList(wString):
-    delim = ", "
-    result = delim.join(sorted(wString.split(", ")))
-    return result
 
 @bot.command(name="showList", aliases=["showlist", "ShowList", "Showlist"], pass_context=True)
 async def showList(ctx):
@@ -110,22 +105,12 @@ async def showList(ctx):
         for anime in mylist.split(","):
             embed.add_field(name=f'{counter}', value=f"{anime}", inline=True)
             counter += 1
-    if len(result) == 0:
-        raise Exception()
-    if len(mylist) == 5:
-        print(mylist)
-        raise Exception()
-        await ctx.send("The list is empty!")
-    else:                                             
-        mylist = mylist[2:-3]
-        mylist = await sortWatchList(mylist)
         print(mylist)
         await ctx.send(embed=embed)
 
 
 @bot.command(name="delAnime", aliases=["Delanime", "DelAnime", "delanime"], pass_context=True)
 async def delAnime(ctx, *, arg):
-    
     my_id = str(ctx.message.author.id)
     cur.execute(f"SELECT animelist FROM watchlist WHERE user_id = {my_id}")
     result = cur.fetchall()
@@ -171,11 +156,13 @@ async def createList(ctx):
         await ctx.send(embed=embed)
         conn.commit()
 
-#Exception Handling
+
+# Exception Handling
 
 @bot.check
 def check_command(ctx):
     return ctx.command.qualified_name
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -195,19 +182,6 @@ async def on_command_error(ctx, error):
         await ctx.send("> Command not found!" + '\n' +
                        "> Use `!help` for list of commands" + '\n' +
                        "> Use `!help <command name>` for specific command details")
-                        "> Use `!createList` to start a list" + '\n')
-    elif isinstance(error, commands.MissingRequiredArgument):
-        if check_command(ctx) == "saveList":
-            await ctx.send("> Incorrect Usage!" + '\n' +
-                            "> Use `!savelist <anime title>` to save an anime to your list" + '\n')
-        if check_command(ctx) == "delAnime":
-            await ctx.send("> Incorrect Usage!" + '\n' +
-                            "> Use `!delanime <anime title>` to delete an anime from your list" + '\n')
-    
-    elif isinstance(error, commands.CommandNotFound):
-        await ctx.send("> Command not found!" + '\n' +
-                        "> Use `!help` for list of commands" + '\n' +
-                        "> Use `!help <command name>` for specific command details")  
     else:
         raise error
 
@@ -249,22 +223,8 @@ async def clearList(ctx):
             if str(reaction.emoji) == "❌":
                 embed.add_field(name="Hmm", value=f"Interaction canceled", inline=False)
                 await ctx.send(embed=embed)
-            cur.execute("""UPDATE watchlist SET animelist= %s WHERE user_id = %s""", ("", author_id))
-            conn.commit()
-            await ctx.send("List cleared!")
     else:
         await ctx.send("You don't have any list saved!")
-
-@bot.command(name="deleteList", aliases=["Deletelist", "DeleteList", "deletelist"], pass_context=True)
-async def deleteList(ctx):
-    author_id = str(ctx.message.author.id)
-    cur.execute(f"select user_id from watchlist where user_id = {author_id}")
-    find_id = cur.fetchall()
-    if len(find_id) != 0:
-        cur.execute(f"DELETE FROM watchlist where user_id = {author_id}")
-        await ctx.send("List deleted!")
-    else:
-        await ctx.send("You don't have a list baka!")
 
 
 @bot.command(name="deleteList", aliases=["Deletelist", "DeleteList", "deletelist"], pass_context=True)
@@ -374,75 +334,6 @@ async def help(ctx, *, arg=None):
         embeddeleteList.add_field(name='Command: deleteList', value='Alias:  `deletelist`, `Deletelist`, `DeleteList`')
         embeddeleteList.add_field(name='Details:', value='`Deletes list from database`', inline=False)
         embeddeleteList.add_field(name='Usage:', value='`!deleteList`', inline=False)
-        
-async def help(ctx, *, arg = None):
-    if(arg == "createList" or arg == "createlist" or arg == "Createlist" or arg == "CreateList"):
-        # await ctx.send("createList Usage: !createList")
-        embedcreateList = discord.Embed(
-        title = '**Help Menu**',
-        description = '',
-        color = discord.Color.blue()
-        )
-        embedcreateList.set_footer(text=f'Requested by - {ctx.author}', icon_url = ctx.author.avatar_url)
-        embedcreateList.add_field(name='Command: createList',value='Alias:  `createlist`, `Createlist`, `CreateList`')
-        embedcreateList.add_field(name='Details:',value='`Creates a list in the database`', inline = False)
-        embedcreateList.add_field(name='Usage:',value='`!createList`', inline = False)
-        await ctx.send(embed=embedcreateList)
-        return
-
-    elif(arg == "saveList" or arg == "savelist" or arg == "Savelist" or arg == "SaveList"):
-        # await ctx.send("saveList Usage: !saveList <anime name>")
-        embedsaveList = discord.Embed(
-        title = '**Help Menu**',
-        description = '',
-        color = discord.Color.blue()
-        )
-        embedsaveList.set_footer(text=f'Requested by - {ctx.author}', icon_url = ctx.author.avatar_url)
-        embedsaveList.add_field(name='Command: saveList',value='Alias:  `savelist`, `Savelist`, `SaveList`')
-        embedsaveList.add_field(name='Details:',value='`Saves anime to list`', inline = False)
-        embedsaveList.add_field(name='Usage:',value='`!saveList <anime name>`', inline = False)
-        await ctx.send(embed=embedsaveList)
-        return
-
-    elif(arg == "showList" or arg == "showlist" or arg == "Showlist" or arg == "ShowList"):
-        # await ctx.send("showList Usage: !showList")
-        embedshowList = discord.Embed(
-        title = '**Help Menu**',
-        description = '',
-        color = discord.Color.blue()
-        )
-        embedshowList.set_footer(text=f'Requested by - {ctx.author}', icon_url = ctx.author.avatar_url)
-        embedshowList.add_field(name='Command: showList',value='Alias:  `showlist`, `Showlist`, `ShowList`')
-        embedshowList.add_field(name='Details:',value='`Shows your list`', inline = False)
-        embedshowList.add_field(name='Usage:',value='`!showList`', inline = False)
-        await ctx.send(embed=embedshowList)
-        return
-
-    elif(arg == "delAnime" or arg == "delanime" or arg == "Delanime" or arg == "DelAnime"):
-        # await ctx.send("delAnime Usage: !delAnime <anime name>")
-        embeddelAnime = discord.Embed(
-        title = '**Help Menu**',
-        description = '',
-        color = discord.Color.blue()
-        )
-        embeddelAnime.set_footer(text=f'Requested by - {ctx.author}', icon_url = ctx.author.avatar_url)
-        embeddelAnime.add_field(name='Command: delAnime',value='Alias:  `delanime`, `Delanime`, `DelAnime`')
-        embeddelAnime.add_field(name='Details:',value='`Deletes an anime from list`', inline = False)
-        embeddelAnime.add_field(name='Usage:',value='`!delAnime <anime name>`', inline = False)
-        await ctx.send(embed=embeddelAnime)
-        return
-
-    elif(arg == "deleteList" or arg == "deletelist" or arg == "Deletelist" or arg == "DeleteList"):
-        embeddeleteList = discord.Embed(
-        title = '**Help Menu**',
-        description = '',
-        color = discord.Color.blue()
-        )
-        embeddeleteList.set_footer(text=f'Requested by - {ctx.author}', icon_url = ctx.author.avatar_url)
-        embeddeleteList.add_field(name='Command: deleteList',value='Alias:  `deletelist`, `Deletelist`, `DeleteList`')
-        embeddeleteList.add_field(name='Details:',value='`Deletes list from database`', inline = False)
-        embeddeleteList.add_field(name='Usage:',value='`!deleteList`', inline = False)
-
         await ctx.send(embed=embeddeleteList)
         return
 
@@ -451,29 +342,29 @@ async def help(ctx, *, arg = None):
         description='Use any of the following commands:',
         color=discord.Color.blue()
     )
-
     embed.set_footer(text=f'Requested by - {ctx.author}', icon_url=ctx.author.avatar_url)
     embed.add_field(name='Commands:',
                     value=' `createList`, `saveList`, `showList`, `delAnime`, `deleteList`, `recommend`, `clearList`')
     embed.add_field(name='Details:', value='`!help <command>`', inline=False)
     await ctx.send(embed=embed)
 
+
 @bot.command(name="poll", aliases=["Poll"], pass_context=True)
-async def poll(ctx,choice1,choice2,*,question):
+async def poll(ctx, choice1, choice2, *, question):
     embed = discord.Embed(
-        title = question,
-        description = f":one: {choice1}\n\n:two: {choice2}",
-        color = discord.Color.blue(),
-        timestamp = datetime.datetime.utcnow()
+        title=question,
+        description=f":one: {choice1}\n\n:two: {choice2}",
+        color=discord.Color.blue(),
+        timestamp=datetime.datetime.utcnow()
     )
-    embed.set_footer(text = f"Poll by {ctx.author.name}")
-    embed.set_thumbnail(url = ctx.author.avatar_url)
-    message = await ctx.send(embed = embed)
+    embed.set_footer(text=f"Poll by {ctx.author.name}")
+    embed.set_thumbnail(url=ctx.author.avatar_url)
+    message = await ctx.send(embed=embed)
     await message.add_reaction("1️⃣")
     await message.add_reaction("2️⃣")
     await asyncio.sleep(5)
 
-    newmessage = await ctx.fetch_message(message.id) 
+    newmessage = await ctx.fetch_message(message.id)
     firstChoice = await newmessage.reactions[0].users().flatten()
     secondChoice = await newmessage.reactions[1].users().flatten()
 
@@ -484,13 +375,13 @@ async def poll(ctx,choice1,choice2,*,question):
         result = choice2
 
     embed2 = discord.Embed(
-        title = question,
-        description = f"Vote has concluded:\nResult : {result}",
-        color = discord.Color.blue(),
-        timestamp = datetime.datetime.utcnow()
+        title=question,
+        description=f"Vote has concluded:\nResult : {result}",
+        color=discord.Color.blue(),
+        timestamp=datetime.datetime.utcnow()
     )
-    embed2.set_footer(text = f"{choice1} || {choice2} ")
-    await newmessage.edit(embed = embed2)
-    
-    
+    embed2.set_footer(text=f"{choice1} || {choice2} ")
+    await newmessage.edit(embed=embed2)
+
+
 bot.run('OTQyMjgwNzE5NjU1Mzk1MzY5.YgiNTg.e1knou32SWUBoL7iY4p6PcKHETQ')
